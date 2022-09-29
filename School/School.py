3ff0b37d5c5e29277c4225a731ad99f40508336d
MOMO <momo@loyalzc.local>
2022-09-29 11:49:59 +0800

    s

--- School/School.py
+++ School/School.py
@@ -6,3 +6,17 @@
  Date：2022/9/29
  Dscription：
 """
+import random
+
+student_names = ['A', 'B', 'C', 'D', 'E']
+suject_names = ['1', '2', '3', '4', '5']
+
+class_score = {}
+for std in student_names:
+    std_score = {}
+    for suj in suject_names:
+        std_score[suj] = random.randint(0,100)
+    class_score[std] = std_score
+
+print(class_score)
+
