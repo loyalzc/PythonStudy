0b6f942f4e53cf03e37be842bdc211166765d10e
Zz1216 <114646729+Zz1216@users.noreply.github.com>
2022-10-01 01:59:40 +0800

    Update School.py

--- School/School.py
+++ School/School.py
@@ -51,21 +51,37 @@ def set_student_info(class_score):
 
 
 print(get_student_score('A', '2'))
+import random
 
-def sort_sum():
-    for name1 in class_score[student_names]:
-        result = 0
-        for name2 in student_names[subject_names]:
-            result = result + name2
+student_names = ['A', 'B', 'C', 'D', 'E']
+suject_names = ['1', '2', '3', '4', '5']
+
+class_score = {}
+for std in student_names:
+    std_score = {}
+    for suj in suject_names:
+        std_score[suj] = random.randint(0,100)
+    class_score[std] = std_score
+
+
+student_sum1 = {}
+for name1 in class_score[student_names]:
+               result = 0
+            for name2 in student_names[subject_names]:
+                result = result + name2
             student_sum1[name1] = result
-            for i in range():
-                array[i] = result
 
-    for i in range(0, len(array)-1):
-            min_index = i
-            for j in range(i+1, len(array)):
-                if array[j] < array[min_index]:
-                    min_index = j
-                    array[i], array[min_index] = array[min_index], array[i]
+rank_score = sorted(student_sum1.items(), key=lambda x: x[1], reverse=true)
 
-     return array
+
+
+def get_student_rank(class_score):
+    """
+    计算所有学生的总分并排序输出
+    :param class_score: 各个学生各个科目分数
+    :return: 按照总分由大到小的顺序排序 姓名和总分
+    """
+    rank_score = {}
+    return rank_score
+
+print(rank_score)
