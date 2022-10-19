1bf772317a0362c483bb59cc8d78ab5005978d00
Cheng <loyal.czhong@gmail.com>
2022-10-19 23:15:51 +0800

    init

--- School/School.py
+++ School/School.py
@@ -18,6 +18,8 @@ for std in student_names:
         std_score[suj] = random.randint(0, 100)
     class_score[std] = std_score
 
+"""
+
 class_score_02 = dict(class_score)
 for std in student_names:
     result = 0
@@ -29,15 +31,12 @@ class_score_03 = dict(class_score)
 
 for std in student_names:
     std_attribute = {}
-    for
-
+    pass
+"""
 
 
 random.randint(0, 20)
 random.choice(['M', 'F'])
-
-
-
 
 def get_student_score(std_name, suj_name):
     """
@@ -49,24 +48,36 @@ def get_student_score(std_name, suj_name):
     score = class_score[std_name][suj_name]
     return score
 
-def get_student_rank(class_score_02):
+def get_student_rank(class_score):
     """
     计算所有学生的总分并排序输出
     :param class_score: 各个学生各个科目分数
     :return: 按照总分由大到小的顺序排序 姓名和总分
     """
-    rank_sort = sorted(class_score_02.item(), key=lambda x: x[1], reverse=true)
-    return rank_sort
-def set_student_info(class_score_3):
+    # rank_sort = sorted(class_score_02.item(), key=lambda x: x[1], reverse=true)
+    for std_name, subj_scores in class_score.items():
+        sum_socre = 0
+        for suj, score in subj_scores.items():
+            sum_socre += score
+        subj_scores["sum_socre"] = sum_socre
+    print(class_score)
+
+
+def set_student_info(class_score):
     """
     新增 学生的 年龄（0-20 随机） 性别（M F 随机）
     :param class_score: 原学生信息
     :return: 原来字典新增后的学生信息 包括（年龄 性别 各个科目分数
     """
-    return class_score_3
+    for std_name, stf_info in class_score.items():
+        stf_info['age'] = random.randint(0, 20)
+        stf_info['gender'] = random.choice(['M', 'F'])
+    return class_score
+
 
 print(get_student_score('A', '2'))
-print(get_student_rank(class_score_02))
-print(get_student_info(class_score_03))
+print(get_student_rank(class_score))
+print(set_student_info(class_score))
+
 
 
