c9ed73ab6a128be83cd5322fc196500a117a6ad8
MOMO <momo@loyalzc.local>
2022-10-20 18:58:13 +0800

    student class

---
+++ School/Student.py
@@ -0,0 +1,17 @@
+# !/usr/bin/python
+# -*- coding: utf8 -*-
+"""
+ Package：
+ User：momo
+ Date：2022/10/20
+ Dscription：
+"""
+
+
+class Student():
+    """
+    学生类
+    """
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
